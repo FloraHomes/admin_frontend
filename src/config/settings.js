8b09e9b291a5ef9741c70785b94052a93635dafb
ibraphem <ibraphem@gmail.com>
2022-12-27 12:16:37 +0100

    settings

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,2 +1,6 @@
-export const BASE_URL = 'http://localhost:7000/api';
-export const SITE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:7000/api';
+// export const SITE_URL = 'http://localhost:3000';
+
+
+export const BASE_URL = "https://flora-homes-backend-new.herokuapp.com/api"
+export const SITE_URL = "https://flora-homes-admin-dashboard.herokuapp.com/"
