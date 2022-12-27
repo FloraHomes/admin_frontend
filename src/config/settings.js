060d063b19c3caf5d5f22e3e4285cf8d5123bfe3
ibraphem <ibraphem@gmail.com>
2022-12-27 15:27:12 +0100

    netlify

--- src/config/settings.js
+++ src/config/settings.js
@@ -3,4 +3,4 @@
 
 
 export const BASE_URL = "https://flora-homes-backend-new.herokuapp.com/api"
-export const SITE_URL = "https://flora-homes-admin-dashboard.herokuapp.com/"
+export const SITE_URL = "florahomes-app.netlify.app"
