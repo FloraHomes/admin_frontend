273deea390683534aa3bdf2830299df1cb107062
ibraphem <ibraphem@gmail.com>
2023-01-10 16:17:54 +0100

    html to pdf

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = 'http://localhost:7000/api';
-// export const SITE_URL = 'http://localhost:3000';
+export const BASE_URL = 'http://localhost:7000/api';
+export const SITE_URL = 'http://localhost:3000';
 
 
-export const BASE_URL = "https://flora-homes-backend-new.herokuapp.com/api"
-export const SITE_URL = "florahomes-app.netlify.app"
+// export const BASE_URL = "https://flora-homes-backend-new.herokuapp.com/api"
+// export const SITE_URL = "florahomes-app.netlify.app"
--- src/layouts/modals/PaymentModal.jsx
+++ src/layouts/modals/PaymentModal.jsx
@@ -83,6 +83,7 @@ const PaymentModal = () => {
     res?.status && dispatch(fetchCustomerPayments());
   };
 
+  console.log(data)
 
 
   return (
