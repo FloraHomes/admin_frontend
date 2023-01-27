cef03ae9c59e1939341f03dec9f02c53f661e3b9
ibraphem <ibraphem@gmail.com>
2023-01-27 16:46:21 +0100

    switch url

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
-export const BASE_URL = 'http://localhost:7000/api';
-export const SITE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:7000/api';
+// export const SITE_URL = 'http://localhost:3000';
 
 
-// export const BASE_URL = "https://florahomes-backend.herokuapp.com/api"
-// export const SITE_URL = "florahomes-app.netlify.app"
+export const BASE_URL = "https://florahomes-backend.herokuapp.com/api"
+export const SITE_URL = "florahomes-app.netlify.app"
